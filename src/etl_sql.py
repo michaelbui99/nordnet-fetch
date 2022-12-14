1078e989dc91b046a36cc9631fb08f298654f295
Michael Bui <michaelbui99@hotmail.com>
2022-12-14 18:55:19 +0100

    Update query name

--- src/etl_sql.py
+++ src/etl_sql.py
@@ -1,4 +1,4 @@
-def insert_latest_performance_tick():
+def insert_latest_performance_tick_date():
     query = "INSERT INTO `nordnetdata.dwh.etl_metadata` (latest_performance_tick_date) SELECT date FROM `nordnetdata.dwh.performance_graph` order by date DESC LIMIT 1"
     # TODO: Use GCP BigQuery client and target etl_metadata table
     print(query)
