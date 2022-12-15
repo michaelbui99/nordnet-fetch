0cc764cdf0beebcfd3a06878b85333543082316a
Michael Bui <michaelbui99@hotmail.com>
2022-12-15 20:18:16 +0100

    Switch between storing to local file or GCP

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+from sys import argv
 from datetime import date
 from session_handler import SessionHandler
 from nordnet_api import get_transactions
@@ -10,26 +11,31 @@ from dotenv import DotEnv
 dotenv = DotEnv()
 dotenv.load()
 
-# TODO: Move configuration to .env / Environment variables
-with open("config.json", "r") as configFile:
-    config = json.load(configFile)
+# Use credentials from environment variables
+sessionHandler = SessionHandler()
+sessionHandler.login()
 
-    # Use credentials from environment variables
-    sessionHandler = SessionHandler()
-    sessionHandler.login()
+# Tranactions returned as Pandas DataFrame
+transactions = get_transactions(session=sessionHandler.get_session())
+# Performance graph returned as dictionary
+performance_graph = get_performance_graph(
+    session=sessionHandler.get_session())
 
-    # Tranactions returned as Pandas DataFrame
-    transactions = get_transactions(session=sessionHandler.get_session())
-    # Performance graph returned as dictionary
-    performance_graph = get_performance_graph(
-        session=sessionHandler.get_session())
+if (argv[1] is None or argv[1] == "local" or argv[1] == "file"):
+    # TODO: Move configuration to .env / Environment variables
+    with open("config.json", "r") as configFile:
+        config = json.load(configFile)
 
-    # Store newest transactions page as both CSV and Excel file
-    transactions_repository = TransactionsRepository(config)
-    transactions_repository.save_transactions(
-        "file", transactions)
+        # Store newest transactions page as both CSV and Excel file
+        transactions_repository = TransactionsRepository(config)
+        transactions_repository.save_transactions(
+            "file", transactions)
 
-    # Store Performance Graph
-    performance_graph_repository = PerformanceGraphRepository(config)
-    performance_graph_repository.save_performance_graph(
-        "file", performance_graph)
+        # Store Performance Graph
+        performance_graph_repository = PerformanceGraphRepository(config)
+        performance_graph_repository.save_performance_graph(
+            "file", performance_graph)
+
+if (argv[1].lower() == "gcp"):
+    # TODO: Use GCP strategy in both repositories
+    print("GCP")
